35242b433354b9a96d5800335d30b40bbe857445
shady <singhshantanu.0210@gmail.com>
2026-08-27 23:03:20 +0530

    feat: define StorageBackend interface for persistent Raft state and log management

--- internal/storage/persistence.go
+++ internal/storage/persistence.go
@@ -1,1 +1,23 @@
 package storage
+
+import (
+	pb "github.com/shantanu-1607/raftra/proto"
+)
+
+// StorageBackend defines the interface for persisting Raft state and logs
+type StorageBackend interface {
+	// Stable state (metadata)
+	SaveTerm(term uint64) error
+	LoadTerm() (uint64, error)
+	SaveVotedFor(candidateID string) error
+	LoadVotedFor() (string, error)
+
+	// Log storage
+	AppendEntries(entries []*pb.LogEntry) error
+	GetEntry(index uint64) (*pb.LogEntry, error)
+	GetEntriesFrom(startIndex uint64) ([]*pb.LogEntry, error)
+	TruncateFrom(index uint64) error // Deletes all entries from index to the end
+	LastIndex() (uint64, error)
+	LastTerm() (uint64, error)
+	LoadAllEntries() ([]*pb.LogEntry, error)
+}
